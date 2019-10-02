f38cacb9f7aaae445fa43fe63ca2f66198d6a501
everett745 <kirillavdonin2014@yandex.ru>
2019-10-02 21:50:56 +0300

    update

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,37 +1,73 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {AuthService} from './auth.service';
+import {User} from '../core/models';
 
-export interface Errors {
-  errors: {[key: string]: string};
-}
+
 
 @Component({
-  selector: 'app-footer',
+  selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
+
 export class AuthComponent implements OnInit{
+
+  @Output() userObj = new EventEmitter<User>();
 
   authType: string;
   login: boolean;
-  errors: Errors = {errors: {}};
-  isSubmitting = false;
+  register: boolean;
+  public MainUser: User;
 
   username: string;
   email: string;
   password: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
     this.route.url.subscribe(data => {
       this.authType = data[data.length - 1].path;
     });
+    if (this.authType === 'register') {
+      this.register = true;
+      this.login = false;
+    } else if (this.authType === 'login') {
+      this.register = false;
+      this.login = true;
+    }
   }
 
   submitForm() {
-    this.isSubmitting = true;
-    this.errors = {errors: {}};
+    if (this.authType === 'register') {
+      this.authService.registrate({
+          username: this.username,
+          email: this.email,
+          password: this.password
+      })
+        .subscribe( user => {
+          console.log('regist: ', user);
+          this.MainUser = user.user;
+          this.authService.mainUser = user.user;
+        });
 
+      this.userObj.emit(this.MainUser);
+
+    } else if (this.authType === 'login') {
+      this.authService.login({
+        username: this.username,
+        email: this.email,
+        password: this.password
+      })
+        .subscribe( user => {
+          console.log('login: ', user);
+          this.MainUser = user.user;
+          this.authService.mainUser = user.user;
+        });
+
+      this.userObj.emit(this.MainUser);
+    }
   }
 }
--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, Input, OnInit} from '@angular/core';
+import {AuthComponent} from '../../auth/auth.component';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
